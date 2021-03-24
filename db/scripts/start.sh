039fd314f908075524d38dfff7a1014618d586e6
Felipe Rodriguez <felipe.rodriguez@lazos.cl>
2021-03-24 15:27:45 -0300

    Initial commit

---
+++ db/scripts/start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Immediately exits if any error occurs during the script
+# execution. If not set, an error could occur and the
+# script would continue its execution.
+set -o errexit
+
+
+# Creating an array that defines the environment variables
+# that must be set. This can be consumed later via arrray
+# variable expansion ${REQUIRED_ENV_VARS[@]}.
+readonly REQUIRED_ENV_VARS=(
+  "POSTGRES_USER")
+
+
+# Main execution:
+# - verifies if all environment variables are set
+# - runs the SQL code to create user and database
+main() {
+  check_env_vars_set
+  init_user_and_db
+}
+
+
+
+# Performs the initialization in the already-started PostgreSQL
+# using the preconfigured POSTGRE_USER user.
+init_user_and_db() {
+  psql -v ON_ERROR_STOP=1 --username postgres <<-EOSQL
+     CREATE DATABASE rpti;
+EOSQL
+}
+
+# Executes the main routine with environment variables
+# passed through the command line. We don't use them in
+# this script but now you know 🤓
+main "$@"
